96c1dc6c4686e067bca7c263a84194819428b7ad
Mauricio S <mauriciosantoshoyos@gmail.com>
2021-03-16 13:24:32 -0400

    fix imports

--- lib/data/db_api.dart
+++ lib/data/db_api.dart
@@ -1,4 +1,4 @@
-import 'package:bh/models/Entry.dart';
+import '../models/Entry.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
--- lib/widgets/AddEntry.dart
+++ lib/widgets/AddEntry.dart
@@ -1,4 +1,4 @@
-import 'package:bh/models/Entry.dart';
+import '../models/Entry.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
